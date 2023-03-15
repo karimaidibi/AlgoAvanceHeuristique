628f4a54185b1c645bc96cc60b9687da75bc1a0c
karimaidibi <karim.aidibi@etu.umontpellier.fr>
2023-03-15 11:47:16 +0100

    start tp

---
+++ TpGrille/src/main/java/Solution.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+
+public class Solution extends ArrayList<Coord> {
+    
+    public Solution(Coord c){
+        this.add(c);
+    }
+    public Solution(){
+
+    }
+
+
+}
